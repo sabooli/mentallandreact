eb73920131d5b28ba5644a65a8c9c4426b0b5c43
Saba <sabooli22@gmail.com>
2023-07-10 02:16:05 +0330

    set cart functions

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Header from "../header";
 import Navbar from "../Navbar";
 import Footer from "../footer";
@@ -7,9 +7,36 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function Cart() {
  const { cartData, dispatch } = useContext(CartContext);
+ const [totalPrice, setTotalPrice] = useState(0);
+ const [coupon, setCoupon] = useState("");
+ const [grandTotal, setGrandTotal] = useState(0);
+
 
 const handleDeleteReservedList = (index, dispatch) => {
   dispatch({ type: "REMOVE_CART_DATA", index });
+};
+
+useEffect(() => {
+  let sum = 0;
+  for (const cart of cartData) {
+    sum += parseFloat(cart.priceEvent);
+  }
+  setTotalPrice(sum);
+
+let couponDiscount = 0;
+  if (coupon === 'SHECODES10') {
+    couponDiscount = sum * 0.1;
+  }
+  const totalWithDiscount = sum - couponDiscount;
+  setGrandTotal(totalWithDiscount);
+}, [cartData, coupon]);
+
+const handleCouponChange = (event) => {
+  setCoupon(event.target.value);
+};
+
+const handleCheckout = () => {
+  console.log('Checkout button clicked!');
 };
   return (
     <div>
@@ -46,21 +73,40 @@ const handleDeleteReservedList = (index, dispatch) => {
       <div className="makefinal text-center">
         <label>
           Sub Total <br />
-          <input type="number" className="totalprice" />
+          <input
+            type="text"
+            inputmode="numeric"
+            className="totalprice"
+            value={totalPrice}
+            readOnly
+          />
         </label>
-        <div>
+        <div class="coupon-wrapper">
           <label>
             Coupon Code <br />
-            <input type="text" className="coupon" />
+            <input
+              type="text"
+              className="coupon"
+              value={coupon}
+              onChange={handleCouponChange}
+            />
           </label>
           <button className="addcoupon">Add Coupon</button>
         </div>
         <label>
           Grand Total <br />
-          <input type="number" className="grandtotal" />
+          <input
+            type="text"
+            inputmode="numeric"
+            className="grandtotal"
+            value={grandTotal}
+            readOnly
+          />
         </label>
         <div>
-          <button className="checkout">Check out</button>
+          <button className="checkout" onClick={handleCheckout}>
+            Check out
+          </button>
         </div>
       </div>
       <Footer />
