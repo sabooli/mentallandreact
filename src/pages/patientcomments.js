d88545a03639931b7158433a702c5313861098f0
Saba <sabooli22@gmail.com>
2023-08-30 16:48:49 +0330

    added api

--- src/pages/patientcomments.js
+++ src/pages/patientcomments.js
@@ -1,6 +1,7 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Zoom, Navigation, Pagination } from "swiper";
+import { useTranslation } from "react-i18next";
 import "swiper/modules/navigation/navigation";
 import "swiper/modules/pagination/pagination";
 import patienta from "../icons/Ellipse 5.svg";
@@ -13,7 +14,18 @@ import {
 
 SwiperCore.use([Zoom, Navigation, Pagination]);
 
-export default function Patientcomments({heading}) {
+export default function Patientcomments({heading, comments}) {
+ const { t, i18n } = useTranslation();
+ const [key, setKey] = useState(0);
+
+ useEffect(() => {
+   setKey((prevKey) => prevKey + 1);
+ }, [comments]);
+
+ if (!comments) {
+   return null;
+ }
+
    const sliderRef = useRef(null);
    const handlePrev = useCallback(() => {
      if (!sliderRef.current) return;
@@ -25,8 +37,8 @@ export default function Patientcomments({heading}) {
      sliderRef.current.swiper.slideNext();
    }, []);
   return (
-    <div>
-      <h1 className="patientSay text-center">{heading}</h1>
+    <div key={key}>
+      <h2 className="patientSay text-center">{heading}</h2>
       <Swiper
         ref={sliderRef}
         zoom={{ maxRatio: 2 }}
@@ -47,34 +59,35 @@ export default function Patientcomments({heading}) {
           },
         }}
       >
-        <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <div className="cardp swiper-slide">
-              <div className="image-content">
-                <div className="card-image">
-                  <img
-                    src={patienta}
-                    alt="patient comment"
-                    className="card-img patient"
-                    loading="lazy"
-                  />
+        {comments.map((comment) => (
+          <SwiperSlide key={comment.id}>
+            <div className="swiper-zoom-container">
+              <div className="cardp swiper-slide">
+                <div className="image-content">
+                  <div className="card-image">
+                    <img
+                      src={`https://portals.mentalland.com/image/users/comments/${comment.img}`}
+                      alt="patient comment"
+                      className="card-img patient"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="patientName text-center">
+                    {" "}
+                    {comment.Fname_Lname}
+                  </div>
+                  <div className="date">
+                    <span>{comment.date}</span>
+                    <span>{comment.role}</span>
+                  </div>
                 </div>
-                <div className="patientName text-center">Benjamin Smith</div>
-                <div className="date">
-                  <span>October 9</span>
-                  <span>Music</span>
+                <div className="card-content">
+                  <p className="comment text-center">{comment.description}</p>
                 </div>
-              </div>
-              <div className="card-content">
-                <p className="comment text-center">
-                  Lorem ipsum dolor sit amet consectetur. Scelerisque risus
-                  nulla viverra orci tristique magna et. Sagittis enim ut
-                  gravida sed volutpat est arcu vitae ....
-                </p>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
         <SwiperSlide>
           <div className="swiper-zoom-container">
             <div className="cardp swiper-slide">
@@ -159,15 +172,30 @@ export default function Patientcomments({heading}) {
           </div>
         </SwiperSlide>
       </Swiper>
-      <div className="text-center mt-5">
-        <IoIosArrowDropleftCircle
-          className="swiper-navigation___prev"
-          onClick={handlePrev}
-        />
-        <IoIosArrowDroprightCircle
-          className="swiper-navigation___next"
-          onClick={handleNext}
-        />
+      <div className="text-center mt-5 swps">
+        {i18n.language === "fa" ? (
+          <div style={{ whiteSpace: "nowrap" }}>
+            <IoIosArrowDroprightCircle
+              className="swiper-navigation___prev"
+              onClick={handlePrev}
+            />
+            <IoIosArrowDropleftCircle
+              className="swiper-navigation___next"
+              onClick={handleNext}
+            />
+          </div>
+        ) : (
+          <div style={{ whiteSpace: "nowrap" }}>
+            <IoIosArrowDropleftCircle
+              className="swiper-navigation___prev"
+              onClick={handlePrev}
+            />
+            <IoIosArrowDroprightCircle
+              className="swiper-navigation___next"
+              onClick={handleNext}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
